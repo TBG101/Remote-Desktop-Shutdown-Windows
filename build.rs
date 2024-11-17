f92bb7e19e0c69c5f567670aa056103a825a8ca1
TBG101 <ziedhrz@gmail.com>
2024-11-17 12:49:08 +0100

    Added support for CMD commands

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
+use embed_resource;
 use std::fs;
 
 fn main() {
+    embed_resource::compile("app_icon.rc", embed_resource::NONE);
+
     fs::copy("icon.ico", "target/release/icon.ico").expect("Failed to copy icon file");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![windows_subsystem = "windows"]
+use std::io::{self};
 use std::net::UdpSocket;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::thread;
+use winreg::enums::*;
+use winreg::RegKey;
 
 fn main() {
     thread::spawn(|| {
@@ -13,26 +16,57 @@ fn main() {
         Ok(w) => app = w,
         Err(_) => panic!("Can't create window!"),
     }
-    // w.set_icon_from_file(&"C:\\Users\\qdot\\code\\git-projects\\systray-rs\\resources\\rust.ico".to_string());
-    // w.set_tooltip(&"Whatever".to_string());
 
-    let _ = app.set_icon_from_file("icon.ico").unwrap();
+    let _ = app.set_icon_from_file("icon.ico");
 
     _ = app.add_menu_item("Quit", |window| {
         window.quit();
         Ok::<_, systray::Error>(())
     });
 
-    // println!("Waiting on message!");
+    add_to_startup().expect("Failed to add to startup");
+
     _ = app.wait_for_message();
 }
 
+fn send_response(socket: &UdpSocket, response: &str, sender: &str) {
+    // Send the response to the sender
+    socket
+        .send_to(response.as_bytes(), sender)
+        .expect("Failed to send response");
+}
+
+fn execute_cmd(cmd: &str) -> io::Result<String> {
+    let output = Command::new("cmd")
+        .args(["/C", cmd])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .output()?;
+
+    if output.status.success() {
+        Ok(String::from_utf8_lossy(&output.stdout).to_string())
+    } else {
+        Err(io::Error::new(
+            io::ErrorKind::Other,
+            String::from_utf8_lossy(&output.stderr).to_string(),
+        ))
+    }
+}
+
+fn excute_shutdown_command() -> String {
+    let output = Command::new("cmd")
+        .args(&["/C", "shutdown /s"])
+        .output()
+        .expect("failed to execute process");
+
+    let result = String::from_utf8_lossy(&output.stdout);
+    return result.to_string();
+}
+
 fn packet_loop() {
-    // Define the address anld port to listen on
     let listen_address = "0.0.0.0";
     let listen_port = 8888;
 
-    // Create a UDP socket
     let udp_socket = UdpSocket::bind(format!("{}:{}", listen_address, listen_port))
         .expect("Failed to bind to address");
 
@@ -40,26 +74,36 @@ fn packet_loop() {
         "Listening for UDP packets on {}:{}",
         listen_address, listen_port
     );
-    // Buffer to store received data
+
     let mut buf = [0; 1024];
+
     loop {
-        // Receive data and the address of the sender
         let (size, sender) = udp_socket
             .recv_from(&mut buf)
             .expect("Failed to receive data");
-        // Convert the received data to a UTF-8 string
-        let message = String::from_utf8_lossy(&buf[0..size]);
+        let mut message = String::from_utf8_lossy(&buf[0..size]).to_string();
 
-        // Print the received message and sender's address
-        println!("Received message '{}' from {}", message, sender);
         if message == "shutdown" {
-            let output = Command::new("cmd")
-                .args(&["/C", "shutdown /s"])
-                .output()
-                .expect("failed to execute process");
-
-            let result = String::from_utf8_lossy(&output.stdout);
-            println!("Command output:\n{}", result);
+            excute_shutdown_command();
+        } else if message.starts_with("cmd") && message.len() > 4 {
+            let cmd = message.replace("cmd ", "");
+            match execute_cmd(&cmd) {
+                Ok(output) => send_response(&udp_socket, &output, &sender.to_string()),
+                Err(e) => eprintln!("Error: {}", e),
+            }
         }
     }
+}
+
+fn add_to_startup() -> io::Result<()> {
+    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+    let run_key = hkcu.open_subkey_with_flags(
+        "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+        KEY_WRITE,
+    )?;
+
+    let exe_path = std::env::current_exe()?.to_string_lossy().to_string();
+
+    run_key.set_value("RemoteShutdown", &exe_path)?;
+    Ok(())
 }
